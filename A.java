7701a480a28c00a63c51489eb3d54ea8ff54bc60
Symbolhallo <1527467911@qq.com>
2016-11-16 10:39:21 +0800

    上传

---
+++ A.java
@@ -0,0 +1,14 @@
+class A {
+	A (){
+		System.out.println("我是构建函数");
+	}
+     void funA (){
+		 System.out.println("没有参数的函数");
+	 }
+	 void funA (int a){
+		 System.out.println("有一个整形的函数");
+	 }
+	 void funA (int a ,double b){
+		 System.out.println("有二个变量的函数");
+	 }
+}
